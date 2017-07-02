01942417a57707fc7d5332b6b531780ca03e7080
Micha <mlk04@wp.pl>
2017-07-02 15:38:00 +0200

    first commit

--- fun.js
+++ fun.js
@@ -1,52 +1,44 @@
 function wypisz()
 			{
-				var liczba1 = document.getElementById("pole1").value;
-				var liczba2 = document.getElementById("pole2").value;
-				var napis = "";
 
-				if(isNaN(liczba1) && isNaN(liczba2))  //sprawdzanie czy obie wartosci zle
+				try
 				{
-					document.getElementById("wynik").innerHTML =
- 					"error: both inputs contain wrong data";
-					return;
-				}
+					var liczba1 = document.getElementById("pole1").value;
+					var liczba2 = document.getElementById("pole2").value;
+					var napis = "";
 
-				if(isNaN(liczba2)) //sprawdzanie czy prawa wartosc zla
-				{
-					document.getElementById("wynik").innerHTML = 				
-					"error: right input contains wrong data";
-					return;
-				}					
-					
-				if(isNaN(liczba1)) //sprawdzanie czy lewa wartosc zla
-				{	
-					document.getElementById("wynik").innerHTML =	
-					"error: left input contains wrong data";
-					return;
-				}
-			
-
+					if(isNaN(liczba1) && isNaN(liczba2))  throw "error: both inputs contain wrong data";
 				
-				if (liczba1<liczba2) //
-				{
-					for (i=liczba1; i<=liczba2; i++)
+					if(isNaN(liczba2)) throw "error: right input contains wrong data";
+											
+					if(isNaN(liczba1)) throw "error: left input contains wrong data";
+						
+					var liczba11 = parseInt(liczba1);
+					var liczba22 = parseInt(liczba2);
+					
+					if (liczba11<liczba22) 
 					{
-						napis = napis + i + " ";
+						for (i=liczba11; i<=liczba22; i++)
+						{
+							napis = napis + i + " ";
+						}
 					}
-				}
 
-				else if (liczba2<liczba1)
-				{					
-					for (i=liczba1; i>=liczba2; i--)
-					{
-						napis = napis + i + " ";
-					}
-				} 
-				else 
-				{
-					document.getElementById("wynik").innerHTML = 
-					"error: no elements";
-					return;	
+					else if (liczba22<liczba11)
+					{					
+						for (i=liczba11; i>=liczba22; i--)
+						{
+							napis = napis + i + " ";
+						}
+					} 
+					else throw "error: no elements";
+
+					document.getElementById("wynik").innerHTML = napis;
 				}
-				document.getElementById("wynik").innerHTML = napis;
+				
+				catch (e)
+				{
+					document.getElementById("wynik").innerHTML = e;	
+				}
+				
 			}
